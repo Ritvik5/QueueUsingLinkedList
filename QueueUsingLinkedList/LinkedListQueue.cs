feeba5bc6350eaebd27178c86b7db80373d7a0f1
Ritvik5 <sharma.ritvik56@gmail.com>
2023-03-14 18:35:31 +0530

    Creating Queue using LinkedList

---
+++ QueueUsingLinkedList/LinkedListQueue.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueUsingLinkedList
+{
+    public class LinkedListQueue
+    {
+        public Node front;
+        public Node rear;
+
+        public LinkedListQueue()
+        {
+            this.front = null;
+            this.rear = null;
+        }
+        public void Enqueue(int data)
+        {
+            Node newNode = new Node(data);
+
+
+            if (this.rear == null)
+            {
+                this.front = newNode;
+                this.rear = newNode;
+            }
+            else
+            {
+
+                this.rear.next = newNode;
+                this.rear = newNode;
+            }
+            Console.WriteLine("{0} inserted into Queue", data);
+        }
+
+        internal void Display()
+        {
+            Node temp = this.front;
+
+            if (temp == null)
+            {
+                Console.WriteLine("The Queue is empty");
+                return;
+            }
+
+            Console.Write("Queue: ");
+            while (temp != null)
+            {
+                Console.Write("{0} ", temp.data);
+                temp = temp.next;
+            }
+            Console.WriteLine();
+        }
+    }
+
+}
---
+++ QueueUsingLinkedList/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace QueueUsingLinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedListQueue queue = new LinkedListQueue();
+            queue.Enqueue(56);
+            queue.Enqueue(30);
+            queue.Enqueue(70);
+            queue.Display();
+        }
+    }
+}
